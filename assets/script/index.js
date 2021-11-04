07ec37a931ba2ce4ff47c658be31524ae6897bf3
M. M <mehtut.m@gmail.com>
2021-11-04 23:57:58 +0700

    update the script
    update the js script to fully update the dashboard

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,15 +1,31 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-    const url = 'assets/data.json';
-    let retrieved;
-    //fetch json data
+    console.log("START");
+
+    function updateDashboard(criteria) {
+        
+        let cardItem = document.querySelectorAll('.card-content')
+
+        const url = 'assets/data.json';
         fetch(url)
         .then(response => response.json())
-        .then(data => retrieved = data)
+        .then(data => {
+            data.forEach(element => {
+                let activityName = element.title.replace(/ /g, "")
+                .toLowerCase();
+                //update the current 
+                let target = document.querySelector(`.${activityName}-card`);
+                //Update the current period hours
+                target.querySelector('.current-count').textContent = element.timeframes[criteria].current;
+                //Update the previus period hours
+                target.querySelector('.previous-count').textContent = element.timeframes[criteria].previous;
+                //update last period description
+                let frequency = criteria === "daily" ? "day" : criteria.replace('ly','');
+                target.querySelector('.report-type').textContent = frequency;
+            })
+        })
         .catch(err => {console.log(`error: ${err}`)});
-
-
-    // Active the selected filter
-    let filterDashboard = document.querySelectorAll('.filter-item');
+    }
 
     function filterSelected() {
         //remove the active state of filter item
@@ -18,16 +34,18 @@ document.addEventListener('DOMContentLoaded', () => {
         this.classList.add('filter-active');
         
         let filterKey = this.text.toLowerCase();
-        console.log(filterKey)
+        //call a function to update the dash board
+        updateDashboard(filterKey);
     }
 
+    // Active the selected filter
+    let filterDashboard = document.querySelectorAll('.filter-item');
+
     filterDashboard.forEach(element => {
+        
         element.addEventListener('click', filterSelected);
     })
 
-    updateDashboard();
-    function updateDashboard() {
-        let criteria = 'weekly';
+    
 
-    }
 })
